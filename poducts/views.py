0c5db050b0f902c4594647aa3a4681047a1a2fad
josevr94 <Josevrw1@gmail.com>
2024-11-06 13:05:04 -0300

    subiendo crud

--- poducts/views.py
+++ poducts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from .form import ProductForm
 # Create your views here.
+
+
+def product_created(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request,'poducts/product_form.html',{'form':form})   
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request,'poducts/product_list',{'products':products}) 
+
+def product_detail(request,id):
+    product = get_object_or_404(Product,pk=id)    
+    return render(request,'poducts/products_detail.html',{'product':product})
+
+def product_update(request,id):
+    product = get_object_or_404(Product,pk=id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST,instance = product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+    return render(request,'poducts/product_form.html',{'form': form})        
